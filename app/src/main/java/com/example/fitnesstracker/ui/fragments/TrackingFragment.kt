c287d3d90693ffcd912f702a76bfb2e64e0eb60b
Vladislav Kutsmai <vladkytsmay@gmail.com>
2022-03-30 14:27:08 +0300

    set up the MapView on TrackingFragment

--- app/src/main/java/com/example/fitnesstracker/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/fragments/TrackingFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import com.example.fitnesstracker.R
 import com.example.fitnesstracker.databinding.FragmentTrackingBinding
 import com.example.fitnesstracker.ui.fragments.base.BaseFragment
+import com.google.android.gms.maps.GoogleMap
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -15,7 +16,17 @@ class TrackingFragment : BaseFragment(R.layout.fragment_tracking) {
     private var _binding: FragmentTrackingBinding? = null
     private val binding get() = _binding!!
 
+    private var map: GoogleMap? = null
+
     override fun setup(savedInstanceState: Bundle?) {
+        setupMapView(savedInstanceState)
+    }
+
+    private fun setupMapView(savedInstanceState: Bundle?) {
+        binding.apply {
+            mapView?.onCreate(savedInstanceState)
+            mapView?.getMapAsync { map = it }
+        }
     }
 
     override fun setupBinding(inflater: LayoutInflater, container: ViewGroup?): View {
@@ -26,5 +37,35 @@ class TrackingFragment : BaseFragment(R.layout.fragment_tracking) {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        binding.mapView?.onStart()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.mapView?.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        binding.mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        binding.mapView?.onLowMemory()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        binding.mapView?.onSaveInstanceState(outState)
     }
 }
