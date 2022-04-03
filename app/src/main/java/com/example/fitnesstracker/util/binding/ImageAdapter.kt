6c917f016f34718841e3ce80350d97a6c5776f34
Vladislav Kutsmai <vladkytsmay@gmail.com>
2022-04-04 01:28:23 +0300

    added the shimmer effect as a secondary placeholder to the item_run image

--- app/src/main/java/com/example/fitnesstracker/util/binding/ImageAdapter.kt
+++ app/src/main/java/com/example/fitnesstracker/util/binding/ImageAdapter.kt
@@ -2,21 +2,29 @@ package com.example.fitnesstracker.util.binding
 
 import android.graphics.drawable.Drawable
 import android.widget.ImageView
-import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.fitnesstracker.R
+import com.facebook.shimmer.Shimmer
+import com.facebook.shimmer.ShimmerDrawable
 
 @BindingAdapter("app:loadImage", "app:placeholder", requireAll = false)
 fun ImageView.loadImage(byteArray: ByteArray?, placeholder: Drawable? = null) {
+    val shimmer = Shimmer.AlphaHighlightBuilder()
+        .setDuration(1800)
+        .setBaseAlpha(0.7f)
+        .setHighlightAlpha(0.6f)
+        .setDirection(Shimmer.Direction.LEFT_TO_RIGHT)
+        .setAutoStart(true)
+        .build()
+
+    val shimmerDrawable = ShimmerDrawable().apply {
+        setShimmer(shimmer)
+    }
+
     Glide.with(this)
         .load(byteArray)
-        .placeholder(
-            placeholder ?: ContextCompat.getDrawable(
-                this.context,
-                R.drawable.no_image_placeholder
-            )
-        )
+        .placeholder(placeholder ?: shimmerDrawable)
         .error(R.drawable.no_image_placeholder)
         .into(this)
 }
