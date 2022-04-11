ffdb0d51f69815f1a9e0aa310c3c49d08723a6a4
Vladislav Kutsmai <vladkytsmay@gmail.com>
2022-04-11 20:21:04 +0300

    added navigation through the settings

--- app/src/main/java/com/example/fitnesstracker/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/fitnesstracker/ui/fragments/SettingsFragment.kt
@@ -2,6 +2,7 @@ package com.example.fitnesstracker.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.navigation.fragment.findNavController
 import com.example.fitnesstracker.databinding.FragmentSettingsBinding
 import com.example.fitnesstracker.ui.fragments.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,7 +11,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class SettingsFragment : BaseFragment<FragmentSettingsBinding>() {
 
     override fun setup(savedInstanceState: Bundle?) {
+        setupButtons()
+    }
 
+    private fun setupButtons() {
+        binding.apply {
+            btnUpdateUser.setOnClickListener {
+                findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToNewUserFragment())
+            }
+            btnChangeStatistics.setOnClickListener {
+                findNavController().navigate(SettingsFragmentDirections.actionSettingsFragmentToSetupStatisticsFragment())
+            }
+        }
     }
 
     override fun setupBinding(inflater: LayoutInflater): FragmentSettingsBinding =
